bc7ae92c9f6ed37d8c2f9578d48fe22a364dd01e
SUDIP10K89 <sudip10k89@gmail.com>
2024-12-01 23:00:21 +0545

    Making UI

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+// IntersectionObserver to detect when section is in view
+const observerOptions = {
+    root: null, // observing relative to the viewport
+    rootMargin: "0px",
+    threshold: 0.3// trigger when 20% of the element is visible
+};
+
+const sectionsToReveal = document.querySelectorAll('.reveal');
+
+// Create the observer
+const revealObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Add the 'revealed' class when the section is in view
+            entry.target.classList.add('revealed');
+            observer.unobserve(entry.target); // Stop observing after the animation
+        }
+    });
+}, observerOptions);
+
+// Start observing each section
+sectionsToReveal.forEach(section => {
+    revealObserver.observe(section);
+});
+
+
+
+// Check if a theme is stored in localStorage, and apply it
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-mode');
+}
+
+// Function to toggle between light and dark mode
+function toggleTheme() {
+    document.body.classList.toggle('light-mode'); // Toggle the 'light-mode' class
+
+    // Save the theme preference in localStorage
+    if (document.body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+
